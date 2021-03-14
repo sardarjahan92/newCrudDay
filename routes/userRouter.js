496c6b1e80c2d8c90a9b677a1856ecba13192c58
sardarjahan92 <80624010+sardarjahan92@users.noreply.github.com>
2021-03-14 20:47:35 +0530

    this is demo
    init commit

---
+++ routes/userRouter.js
@@ -0,0 +1,11 @@
+const router=require('express').Router()
+const userCtrl=require('../controllers/userCtrl')
+const auth=require("../middleware/auth")
+//Register User
+router.post('/registration',userCtrl.registerUser)
+//Login User
+router.post('/login',userCtrl.loginUser)
+//verify Token
+router.get('/verify',userCtrl.verifiedToken)
+
+module.exports=router
